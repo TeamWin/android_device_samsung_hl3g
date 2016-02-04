2bfde85458d8a047acacd3eb35902ffbff53f645
Aman Kumar <amankumarmicro@gmail.com>
2016-02-04 16:50:21 +0530

    twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,7 @@ BOARD_KERNEL_CMDLINE :=
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/hl3g
-TARGET_KERNEL_CONFIG := cyanogenmod_exynos5260-hl3gxx_00_defconfig
-
+TARGET_KERNEL_CONFIG := recovery_exynos5260-hl3gxx_00_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
 
 # Partitions
@@ -44,14 +43,18 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_INITRC := device/samsung/hl3g/recovery/recovery.rc
+#TARGET_RECOVERY_INITRC := device/samsung/hl3g/recovery/recovery.rc
 TARGET_RECOVERY_FSTAB := device/samsung/hl3g/recovery/twrp.fstab
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/hl3g/recovery/recovery_keys.c
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/hl3g/recovery/recovery_keys.c
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/exynos-dwc3.0/exynos-ss-udc.0/gadget/lun0/file"
+BOARD_USES_MMCUTILS := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+#BOARD_RECOVERY_SWIPE := true
 
 # TWRP Specific
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 DEVICE_RESOLUTION := 720x1280
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
@@ -59,8 +62,6 @@ TW_INTERNAL_STORAGE_PATH := /data/media
 TW_INTERNAL_STORAGE_MOUNT_POINT := sdcard
 TW_EXTERNAL_STORAGE_PATH := /external_sd
 TW_EXTERNAL_STORAGE_MOUNT_POINT := external_sd
-TW_NO_USB_STORAGE := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
